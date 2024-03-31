02658722ed5ad9daa329212dd29175bd9a22c112
Yev <str.lucky86@gmail.com>
2024-03-31 13:36:17 -0400

    added GameType and Participants to Tournament.

--- wcc.core/Infrastructure/Tournament.cs
+++ wcc.core/Infrastructure/Tournament.cs
@@ -9,8 +9,10 @@ namespace wcc.core.Infrastructure
     public class Tournament
     {
         public long TournamentId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public GameType GameType { get; set; }
+        public List<long>? Participants { get; set; }
     }
 }
